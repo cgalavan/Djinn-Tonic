ba57204daa37638fdae9a541903669a1286cd89b
Ryan Swann <RyanSwann1992@gmail.com>
2020-10-18 20:13:55 +0100

    Initial Ladder implementation

--- Djinn & Tonic/Assets/Player.cs
+++ Djinn & Tonic/Assets/Player.cs
@@ -9,20 +9,18 @@ public class Player : MonoBehaviour
     private float m_movementSpeed;
     [SerializeField]
     private float m_jumpSpeed;
-    [SerializeField]
-    private Vector3 m_velocity;
-    [SerializeField]
-    private bool m_smoothMovement = false;
-    [SerializeField]
-    private float m_smoothMovementSlowdown;
 
     private bool m_onGround;
+    private bool m_onLadder;
     private Rigidbody m_rigidBody;
 
     private void Awake()
     {
         m_rigidBody = GetComponent<Rigidbody>();
         Assert.IsNotNull(m_rigidBody);
+
+        m_onGround = false;
+        m_onLadder = false;
     }
 
     // Start is called before the first frame update
@@ -39,21 +37,48 @@ public class Player : MonoBehaviour
         }
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Ladder"))
+        {
+            m_rigidBody.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            m_onLadder = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Ladder"))
+        {
+            m_onLadder = false;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if(!m_smoothMovement)
+        if(m_onLadder)
         {
-            m_velocity.x = 0.0f;
+            m_rigidBody.velocity = new Vector3(m_rigidBody.velocity.x, 0.0f, m_rigidBody.velocity.z);
         }
+        m_rigidBody.velocity = new Vector3(0.0f, m_rigidBody.velocity.y, m_rigidBody.velocity.z);
+        m_rigidBody.useGravity = !m_onLadder;
 
         if (Input.GetKey(KeyCode.D))
         {
-            m_velocity += Vector3.right * m_movementSpeed;
+            m_rigidBody.velocity += Vector3.right * m_movementSpeed;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            m_velocity -= Vector3.right * m_movementSpeed;
+            m_rigidBody.velocity -= Vector3.right * m_movementSpeed;
+        }
+        if(m_onLadder && Input.GetKey(KeyCode.W))
+        {
+            m_rigidBody.velocity += Vector3.up * m_movementSpeed * 0.5f;
+        }
+        if(m_onLadder && Input.GetKey(KeyCode.S))
+        {
+            m_rigidBody.velocity += Vector3.down * m_movementSpeed * 0.5f;
         }
         if (Input.GetKeyDown(KeyCode.Space) && m_onGround)
         {
@@ -61,15 +86,8 @@ public class Player : MonoBehaviour
             m_onGround = false;
         }
 
-        transform.position += m_velocity * Time.deltaTime;
-
-        if(m_smoothMovement)
-        {
-            m_velocity *= m_smoothMovementSlowdown;
-            if (Mathf.Abs(m_velocity.x) <= 0.2f)
-            {
-                m_velocity.x = 0.0f;
-            }
-        }
+        transform.position += m_rigidBody.velocity * Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0.0f);
+        transform.rotation = Quaternion.identity;
     }
 }
